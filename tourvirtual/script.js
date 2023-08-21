2277416d0b863a18cdf2852bc4b030d9eeb53b9b
Rodrigosein <rodrigosein@gmail.com>
2023-08-21 10:36:02 -0300

    Create script.js

---
+++ tourvirtual/script.js
@@ -0,0 +1,3 @@
+function voltar() {
+    window.location.href = "https://www.flipbookpdf.net/web/site/8af664c3675ae5b9a809a60fc178c361170ba623FBP28246608.pdf.html#page/30"; // Substitua pelo link desejado
+  }
